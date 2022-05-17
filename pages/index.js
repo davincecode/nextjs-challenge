010266ef331a6cdb3547d5737a220d35df7340eb
davincecode <hyperscript.ca@gmail.com>
2022-05-16 20:44:03 -0600

    Initial Nextjs Tailwind Setup

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <h1 className="text-7xl font-bold">Hello world!</h1>
+      </div>
+    </>
+  )
+}

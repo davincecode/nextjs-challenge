a89eab92f39c285202a59afd99f1af1a9b2728d4
davincecode <hyperscript.ca@gmail.com>
2022-05-17 03:34:23 -0600

    Update contents in price components

--- components/PriceList.jsx
+++ components/PriceList.jsx
@@ -6,12 +6,13 @@ function PriceList() {
       
   <section className="py-6 dark:bg-coolGray-800 dark:text-coolGray-50">
 	<div className="container mx-auto p-4 sm:p-10">
-		<div className="mb-16 space-y-4 text-center">
-			<h1 className="text-4xl font-semibold leading-tight">Pricing</h1>
-			<p className="px-4 sm:px-8 lg:px-24">Sunt suscipit eaque qui iure unde labore numquam iusto alias explicabo, pariatur ipsam, cupiditate aliquid modi?</p>
+          <div className="mb-16 space-y-4 text-center">
+            <label>PRICING</label>
+			<h1 className="text-4xl font-semibold leading-tight">Pricing tiers, for all contractors.</h1>
+			<p className="px-4 sm:px-8 lg:px-24">Choose an affordable plan that&apos;s packed with the best features for engaging your audience, creating customer loyalty, and driving sales.</p>
 			<div>
-				<button className="px-4 py-1 font-semibold border rounded-l-lg dark:bg-violet-400 dark:border-violet-400 dark:text-coolGray-900">Monthly</button>
-				<button className="px-4 py-1 border rounded-r-lg dark:border-violet-400">Annually</button>
+				<button className="px-4 py-1 font-semibold border rounded-l-lg dark:bg-violet-400 dark:border-violet-400 dark:text-coolGray-900">Monthly billing</button>
+				<button className="px-4 py-1 border rounded-r-lg dark:border-violet-400">Yearly billing</button>
 			</div>
     </div>
     <div className="grid max-w-md grid-cols-1 gap-6 mx-auto auto-rows-fr lg:max-w-full lg:gap-2 xl:gap-6 lg:grid-cols-3">

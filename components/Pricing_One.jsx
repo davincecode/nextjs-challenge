eb873b79616b5b142091eea96b354e6e8ad49d98
davincecode <hyperscript.ca@gmail.com>
2022-05-18 15:26:35 -0600

    Clean up comments and minor fix

--- components/Pricing_One.jsx
+++ components/Pricing_One.jsx
@@ -8,8 +8,8 @@ const [subscription, setSubscription] = useState(true);
 
   return (
     <>  
-    <section className="sans py-16 antialiased bg-primary-grey md:w-3/5 container mx-auto">
-      <div className="mb-16 space-y-4 text-center">
+    <section className="sans py-16 antialiased bg-primary-grey container mx-auto">
+      <div className="mb-16 space-y-8 text-center">
         <label className='text-[#2563eb]'>PRICING</label>
           <h1 className="text-4xl font-semibold leading-tight">Pricing tiers, for all contractors.</h1>
             <p className="px-4 sm:px-8 lg:px-24">Choose an affordable plan that&apos;s packed with the best features for engaging your audience, creating customer loyalty, and driving sales.</p>
@@ -20,9 +20,9 @@ const [subscription, setSubscription] = useState(true);
           </div>
       </div>
 
-      <section className="flex flex-col lg:flex-row items-center lg:justify-center w-full lg:px-10 py-12">
+      <section className="flex flex-col lg:flex-row items-center lg:justify-center w-full lg:px-10 py-4">
           <article className="hover:shadow-2xl hover:bg-[#f7f8f9] cursor-pointer duration-300 w-4/5 lg:w-custom mb-10 lg:px-8 py-14 text-center text-primary-dark bg-primary-white border rounded-2xl lg:rounded-r-none lg:rounded-l-2xl shadow-md min-h-[520px] flex-1">
-            <h5 className="font-bold text-base">Starter</h5>
+            <h5 className="font-bold text-2xl">Starter</h5>
               <h2 className="pb-4 flex justify-center flex-col font-bold ">
                 <span className="text-6xl py-4">FREE</span>
                 <span className="text-lg font-normal flex-wrap px-4">Get started with Renotag, absolutely free!</span>
@@ -38,11 +38,11 @@ const [subscription, setSubscription] = useState(true);
           </article>
           
           <article className="hover:shadow-2xl cursor-pointer duration-300 lg:w-custom w-4/5 mb-10 py-16 lg:px-12 text-center text-white bg-[#2d68eb] rounded-3xl drop-shadow-2xl z-10 min-h-[600px] flex-1" >
-                <h5 className="font-bold text-base ">Pro</h5>
-                <small className='absolute top-0 bg-[#f97315] px-8 py-1 rounded-full -translate-y-1/2'>Most Popular</small>
+                <h5 className="font-bold text-2xl ">Pro</h5>
+                <small className='absolute top-0 bg-[#f97315] px-12 py-3 rounded-full -translate-y-1/2'>Most Popular</small>
                 <div className="flex-col font-bold pb-4 mt-2 flex justify-center">
                   <div className='flex flex-row justify-center items-end py-4'>
-                    <span className="text-6xl ">${ subscription ? 99 : 79 }</span>
+                <span className="text-6xl ">${ subscription ? 99 : 79 }</span>
                     <small>/Month</small>
                   </div>
                   <span className="text-lg font-normal flex-wrap px-4">A plan that suits most general contractor&apos;s needs.</span>
@@ -59,7 +59,7 @@ const [subscription, setSubscription] = useState(true);
           </article>
           
           <article className="hover:shadow-2xl hover:bg-[#f7f8f9] cursor-pointer duration-300 bg-white w-4/5 lg:w-custom mb-10 lg:px-4 py-10 text-center text-primary-dark rounded-2xl lg:rounded-l-none lg:rounded-r-2xl border shadow-md min-h-[520px] flex-1">
-            <h5 className="font-bold text-base">Enterprise</h5>
+            <h5 className="font-bold text-2xl">Enterprise</h5>
                 <h2 className="flex flex-col justify-center pb-4 font-bold ">
                 <div className='flex flex-row justify-center items-end py-4'>
                     <span className="text-6xl">${ subscription ? 129 : 105 }</span>
--- components/Pricing_Two.jsx
+++ components/Pricing_Two.jsx
@@ -12,10 +12,9 @@ function Pricing_Two() {
 
   return (
     <>
-     <div className='w-full h-full bg-[#f7f8f9]'>
-        <section className='md:w-5/6 lg:w-3/5 mx-auto p-4 sm:p-10 '>
-            <div className="py-6 dark:bg-coolGray-800 dark:text-coolGray-50">
-              <div className="container mx-auto p-4 sm:p-10">
+     <div className='w-5/6 md:w-2/3 mx-auto h-full'>
+        <section className='container mx-auto p-0 sm:py-10'>
+            <div className="dark:bg-coolGray-800 dark:text-coolGray-50">
                 <div className="mb-16 space-y-4 text-center">
                   <label className='text-[#2563eb]'>PRICING</label>
                   <h1 className="text-4xl font-semibold leading-tight">Pricing tiers, for all contractors.</h1>
@@ -25,47 +24,49 @@ function Pricing_Two() {
                     <button className={subscription ? monthly : yearly } onClick={() => setSubscription(false)}>Annually</button>
                   </div>
                 </div>
-              <div className="grid max-w-xl grid-cols-1 gap-6 mx-auto auto-rows-fr lg:max-w-full lg:gap-2 xl:gap-6 lg:grid-cols-3">
+              <div className="grid max-w-xl grid-cols-1 mx-auto auto-rows-fr lg:max-w-full lg:gap-2 xl:gap-6 lg:grid-cols-3">
               
               { price.map((item) => (
-                  <div className="relative z-0 flex flex-col items-center hover:bg-[#f2f2f2] transition ease-linear cursor-pointer p-12 border shadow-xl rounded-md flex-1" key={item.id}>
-                    <span className="absolute top-0 px-12 pt-1 pb-2 font-medium rounded-b-2xl dark:bg-[#2563eb] dark:text-[#fff]">{ item.name }</span>
-                      <p className="my-12 text-4xl font-bold dark:text-[#2563eb]">{ subscription ? item.monthlyprice : item.annualprice }</p>
-                      <ul className="flex-1 space-y-2">
-                        <li className="flex items-center space-x-2">
+                <>
+                <div className="relative z-0 flex md:mb-0 mb-10 flex-col items-center hover:bg-[#f2f2f2] transition ease-linear cursor-pointer p-12 border shadow-xl rounded-3xl flex-1" key={ item.id }>
+                  
+                    <span className="absolute top-0 min-w-full text-xl text-center pt-8 pb-8 font-medium rounded-t-3xl dark:bg-[#2563eb] dark:text-[#fff]">{ item.name }</span>
+                  <p className="mt-20 mb-8 text-4xl font-bold dark:text-[#2563eb]">{ subscription ? item.monthlyprice : item.annualprice }<small className='text-sm'>{ item.subs }</small></p>
+                      <ul className="flex-1 space-y-4">
+                        <li className="flex items-center pb-8">
                         <span>{ item.description }</span>
                         </li>
                     <div>
                       <ul>{ item.details.map(
-                          (detail) => (
+                        (detail) => (
                             <li className="flex items-center space-x-2" key={ detail }>
                               <BiCheckCircle /><span>{ detail }</span>
                             </li>
                           ))}
                           </ul>
                         </div>
-                  </ul>
+                      </ul>
+
                   <section>
-                    <div className="flex flex-wrap justify-center py-20 text-gray-300 my-5">
-                      <a href="!" className="button button--winona p-0 bg-[#2563eb] hover:bg-[#f97315] hover:text-white text-[#fff] relative block focus:outline-none border-2 border-solid rounded-lg text-sm text-center font-semibold uppercase tracking-widest align-middle overflow-hidden" data-text="TRY IT NOW">
+                    <div className="flex flex-wrap justify-center py-4 text-gray-300 my-8">
+                      <a href="!" className="button button--winona p-0 bg-[#2563eb] hover:bg-[#f97315] hover:text-white text-[#fff] relative block focus:outline-none border-2 border-solid rounded-lg text-sm text-center font-semibold uppercase tracking-widest align-middle overflow-hidden" data-text="TRY IT">
                         <span className="align-middle block">
                           {item.button}
                         </span>
                       </a>
                     </div>
                   </section>
-    
-                </div>
+                  </div>
+                </>
                 ))}
           </div>
         </div>
-          </div>
         </section>
-        <div className="wave">
+      </div>
+      <div className="wave">
         <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
         <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill"></path>
         </svg>
-        </div>
       </div>
     </>
   )
